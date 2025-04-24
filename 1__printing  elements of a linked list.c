954115a675a241a4191d7ebabfdd361e77c04cba
rk <englishenglishenglish@proton.me>
2025-04-24 16:13:34 +0000

    Add files via upload

---
+++ 1__printing  elements of a linked list.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+//defining the structure 
+struct node{
+    int data;
+    struct node* next;
+};
+
+
+
+//logic for creating a node;
+struct node* crt_node(int data){
+
+        struct node* new_node=(struct node*)malloc(sizeof(struct node));
+
+        new_node->data=data;
+        new_node->next=NULL;
+        return new_node;
+}
+
+
+
+
+//logic for adding a node at the last position of a node
+void add( struct node** head_point,int new_data){
+
+    struct node* newnode=crt_node(new_data);
+    struct node* last=*head_point;
+
+
+    if(*head_point==NULL){
+        *head_point=newnode;
+        return;
+    }
+
+
+    while(last->next!=NULL){
+        last=last->next;
+    }
+
+
+    last->next=newnode;
+
+}
+
+//logic for printing a linked list
+void print(struct node* node){
+        while(node!=NULL){
+            printf("%d->",node->data);
+            node=node->next;
+        }
+}
+
+
+
+
+int main(){
+    struct node* head=NULL;
+    add(&head,2);
+    add(&head,4);
+    add(&head,5);
+    add(&head,9);
+
+
+    print(head);
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
