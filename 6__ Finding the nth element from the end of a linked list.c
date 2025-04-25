15e1577c6ce718dde858ed2f9f7dd857d5418a25
rk <englishenglishenglish@proton.me>
2025-04-25 14:16:18 +0000

    Add files via upload

---
+++ 6__ Finding the nth element from the end of a linked list.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+//defining the structure 
+struct node{
+    int data;
+    struct node* next;
+};
+
+
+
+//logic for creating a node;
+struct node* crt_node(int data){
+
+        struct node* new_node=(struct node*)malloc(sizeof(struct node));
+
+        new_node->data=data;
+        new_node->next=NULL;
+        return new_node;
+}
+
+
+
+
+//logic for adding a node at the last position of a node
+void add( struct node** head_point,int new_data){
+
+    struct node* newnode=crt_node(new_data);
+    struct node* last=*head_point;
+
+
+    if(*head_point==NULL){
+        *head_point=newnode;
+        return;
+    }
+
+
+    while(last->next!=NULL){
+        last=last->next;
+    }
+
+
+    last->next=newnode;
+
+}
+
+
+//logic for finding the nth node from the end;
+ int count=0;
+void trvrs_nod(struct node* node,int n){
+    
+        if(node==NULL)
+        return;
+
+        
+
+
+
+        trvrs_nod(node->next,n);
+        count++;
+        
+
+        if(count==n){
+            printf("the %dth node from the end is:%d\n",n,node->data);
+        }
+        
+        
+}
+
+
+
+
+int main(){
+    struct node* head=NULL;
+    add(&head,2);
+    add(&head,4);
+    add(&head,5);
+    add(&head,9);
+
+    int n;
+    scanf("%d",&n);
+
+
+    count=0;
+    trvrs_nod(head,n);
+
+    if(count<n){
+        printf("the list has fewer than %d nodes ",n);
+    }
+    
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
